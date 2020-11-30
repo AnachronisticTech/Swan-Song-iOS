7307e6d851867ca378221c075d32980a39d77626
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2020-11-30 14:47:25 +0200

    Rebuilt song library view controller

--- SwanSong/SongLibraryViewController.swift
+++ SwanSong/SongLibraryViewController.swift
@@ -9,56 +9,67 @@
 import UIKit
 import MediaPlayer
 
-class SongLibraryViewController: SwanSongViewController, UITableViewDelegate {
-    
-    @IBOutlet weak var listView: UITableView!
+class SongLibraryViewController: UITableViewController {
 
-    /// Load tracks from library
     var library = [MPMediaItem]()
-    
+    var collections = [MPMediaItemCollection]()
+    var sections = [MPMediaQuerySection]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        /// Ensure app is authorised, then load library
-        checkAuthorisation(self) { self.librarySetup() }
-        
-        /// Set list view properties
-        listView.dataSource = self
-        listView.delegate = self
-        listView.register(UINib(nibName: "ArtDetailTableCellSmall", bundle: nil), forCellReuseIdentifier: "track")
-    }
-    
-    /// Load songs from library
-    func librarySetup() {
-        let query = MPMediaQuery.songs()
-        query.addFilterPredicate(filterLocal)
-        library = query.items ?? []
-        
-        listView.reloadData()
-    }
-    
-}
 
-extension SongLibraryViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return library.count
+        /// Ensure app is authorised, then load library
+        checkAuthorisation(self) {
+            /// Load tracks from library
+            let query = MPMediaQuery.songs()
+            query.addFilterPredicate(filterLocal)
+            self.library = query.items ?? []
+            self.collections = query.collections ?? []
+            self.sections = query.collectionSections ?? []
+            self.tableView.reloadData()
+        }
+
+        /// Set list view properties
+        tableView.register(UINib(nibName: "ArtDetailTableCellSmall", bundle: nil), forCellReuseIdentifier: "track")
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let track = library[indexPath.row]
-        let cell = listView.dequeueReusableCell(withIdentifier: "track", for: indexPath) as! SingleArtDetailTableViewCell
-        cell.title?.text = track.title ?? ""
-        let time = Formatter.string(from: track.playbackDuration)
-        cell.detail?.text = "\(time ?? "--:--") - \(track.artist ?? "")"
-        cell.artwork?.image = track.artwork?.image(at: CGSize(width: 80, height: 80)) ?? UIImage(named: "blank_artwork")
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section].title
+    }
+
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return sections.map { $0.title }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].range.length
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let index = sections[indexPath.section].range.lowerBound + indexPath.row
+        let track = collections[index].representativeItem
+        let cell = tableView.dequeueReusableCell(withIdentifier: "track", for: indexPath) as! SingleArtDetailTableViewCell
+        cell.title?.text = track?.title ?? ""
+        let time = Formatter.string(from: track?.playbackDuration ?? 0)
+        cell.detail?.text = "\(time ?? "--:--") - \(track?.artist ?? "")"
+        cell.artwork?.image = track?.artwork?.image(at: CGSize(width: 80, height: 80)) ?? UIImage(named: "blank_artwork")
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        Player.play(library, skipping: indexPath.row)
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = sections[indexPath.section].range.lowerBound + indexPath.row
+        Player.play(library, skipping: index)
         performSegue(withIdentifier: "ToPlayer", sender: self)
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
 }
