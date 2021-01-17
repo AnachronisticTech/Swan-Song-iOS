1b127f3b03a0b2d4be4170ba6c0d9589a556a0ce
Daniel Marriner <danielmarriner@anachronistic-tech.co.uk>
2021-01-17 17:56:14 +0200

    Renamed variables and moved Flow class to UserInterface.swift

--- SwanSong/SwanSongCollectionViewController.swift
+++ SwanSong/SwanSongCollectionViewController.swift
@@ -25,7 +25,7 @@ class SwanSongCollectionViewController: UICollectionViewController, SwanSongCont
         willSet {
             DispatchQueue.main.async { [self] in
                 collectionView.reloadData()
-                collectionView.setCollectionViewLayout(newValue ? flowLayout : listLayout, animated: false)
+                collectionView.setCollectionViewLayout(newValue ? gridLayout : listLayout, animated: false)
                 collectionView.collectionViewLayout.invalidateLayout()
             }
             UserDefaults.standard.set(newValue, forKey: "\(identifier)LibraryIsCollectionViewVisible")
@@ -33,7 +33,7 @@ class SwanSongCollectionViewController: UICollectionViewController, SwanSongCont
         }
     }
 
-    var flowLayout: UICollectionViewLayout {
+    var gridLayout: UICollectionViewLayout {
         let layout = UICollectionViewFlowLayout()
         let itemsPerRow = 2
         let sectionInsets = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
@@ -48,8 +48,8 @@ class SwanSongCollectionViewController: UICollectionViewController, SwanSongCont
         return layout
     }
 
-    var listLayout: CustomFlowLayout {
-        let layout = CustomFlowLayout()
+    var listLayout: ListFlowLayout {
+        let layout = ListFlowLayout()
         layout.sectionInsetReference = .fromContentInset
         layout.sectionHeadersPinToVisibleBounds = true
         layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
@@ -147,34 +147,5 @@ class SwanSongCollectionViewController: UICollectionViewController, SwanSongCont
         selected = sections[indexPath.section].range.lowerBound + indexPath.row
         performSegue(withIdentifier: "ToAlbum", sender: self)
         collectionView.deselectItem(at: indexPath, animated: true)
-    }
-}
-
-/// Code courtesy of Imanou Petit on StackOverflow
-/// https://stackoverflow.com/questions/44187881/uicollectionview-full-width-cells-allow-autolayout-dynamic-height
-final class CustomFlowLayout: UICollectionViewFlowLayout {
-    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
-        let layoutAttributesObjects = super.layoutAttributesForElements(in: rect)?.map{ $0.copy() } as? [UICollectionViewLayoutAttributes]
-        layoutAttributesObjects?.forEach({ layoutAttributes in
-            if layoutAttributes.representedElementCategory == .cell {
-                if let newFrame = layoutAttributesForItem(at: layoutAttributes.indexPath)?.frame {
-                    layoutAttributes.frame = newFrame
-                }
-            }
-        })
-        return layoutAttributesObjects
-    }
-
-    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
-        guard let collectionView = collectionView else {
-            fatalError()
-        }
-        guard let layoutAttributes = super.layoutAttributesForItem(at: indexPath)?.copy() as? UICollectionViewLayoutAttributes else {
-            return nil
-        }
-
-        layoutAttributes.frame.origin.x = sectionInset.left
-        layoutAttributes.frame.size.width = collectionView.safeAreaLayoutGuide.layoutFrame.width - sectionInset.left - sectionInset.right
-        return layoutAttributes
     }
 }
--- SwanSong/UserInterface.swift
+++ SwanSong/UserInterface.swift
@@ -148,3 +148,32 @@ enum TintColor: String, CaseIterable {
         }
     }
 }
+
+/// Code courtesy of Imanou Petit on StackOverflow
+/// https://stackoverflow.com/questions/44187881/uicollectionview-full-width-cells-allow-autolayout-dynamic-height
+final class ListFlowLayout: UICollectionViewFlowLayout {
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        let layoutAttributesObjects = super.layoutAttributesForElements(in: rect)?.map{ $0.copy() } as? [UICollectionViewLayoutAttributes]
+        layoutAttributesObjects?.forEach({ layoutAttributes in
+            if layoutAttributes.representedElementCategory == .cell {
+                if let newFrame = layoutAttributesForItem(at: layoutAttributes.indexPath)?.frame {
+                    layoutAttributes.frame = newFrame
+                }
+            }
+        })
+        return layoutAttributesObjects
+    }
+
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let collectionView = collectionView else {
+            fatalError()
+        }
+        guard let layoutAttributes = super.layoutAttributesForItem(at: indexPath)?.copy() as? UICollectionViewLayoutAttributes else {
+            return nil
+        }
+
+        layoutAttributes.frame.origin.x = sectionInset.left
+        layoutAttributes.frame.size.width = collectionView.safeAreaLayoutGuide.layoutFrame.width - sectionInset.left - sectionInset.right
+        return layoutAttributes
+    }
+}
